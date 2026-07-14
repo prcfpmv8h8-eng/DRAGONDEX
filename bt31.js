fdfb4ad7aea434cf886e6e80ab741025c8cda0a9
prcfpmv8h8-eng <prcfpmv8h8@privaterelay.appleid.com>
2026-07-14 11:23:50 +0300

    Add files via upload

---
+++ bt31.js
@@ -0,0 +1,320 @@
+window.DRAGONDEX_DATA = {
+  "set": {
+    "code": "BT31",
+    "name": "Impact Beyond Dimensions",
+    "recordCount": 31,
+    "isComplete": false
+  },
+  "cards": [
+    {
+      "id": "BT31-001",
+      "number": "BT31-001",
+      "name": "Son Goku & Vegeta",
+      "rarity": "UC",
+      "color": "Red",
+      "type": "LEADER",
+      "variant": "Regular",
+      "verified": true
+    },
+    {
+      "id": "BT31-002",
+      "number": "BT31-002",
+      "name": "Janemba",
+      "rarity": "UC",
+      "color": "Red",
+      "type": "LEADER",
+      "variant": "Regular",
+      "verified": true
+    },
+    {
+      "id": "BT31-003",
+      "number": "BT31-003",
+      "name": "SS Gogeta, Invincible Power",
+      "rarity": "R",
+      "color": "Red",
+      "type": "Z-BATTLE",
+      "variant": "Regular",
+      "verified": true
+    },
+    {
+      "id": "BT31-004",
+      "number": "BT31-004",
+      "name": "Phantom Shift -Disintegration-",
+      "rarity": "C",
+      "color": "Red",
+      "type": "Z-EXTRA",
+      "variant": "Regular",
+      "verified": true
+    },
+    {
+      "id": "BT31-005",
+      "number": "BT31-005",
+      "name": "Phantom Shift -Restoration-",
+      "rarity": "C",
+      "color": "Red",
+      "type": "Z-EXTRA",
+      "variant": "Regular",
+      "verified": true
+    },
+    {
+      "id": "BT31-006",
+      "number": "BT31-006",
+      "name": "SS3 Gotenks, Wild Rampage",
+      "rarity": "SR",
+      "color": "Red",
+      "type": "BATTLE",
+      "variant": "Regular",
+      "verified": true
+    },
+    {
+      "id": "BT31-007",
+      "number": "BT31-007",
+      "name": "SS3 Son Goku, Struggle in Another World",
+      "rarity": "C",
+      "color": "Red",
+      "type": "BATTLE",
+      "variant": "Regular",
+      "verified": true
+    },
+    {
+      "id": "BT31-008",
+      "number": "BT31-008",
+      "name": "Son Goku, Other World Warrior",
+      "rarity": "C",
+      "color": "Red",
+      "type": "BATTLE",
+      "variant": "Regular",
+      "verified": true
+    },
+    {
+      "id": "BT31-009",
+      "number": "BT31-009",
+      "name": "Son Goku, Training in Another World",
+      "rarity": "UC",
+      "color": "Red",
+      "type": "BATTLE",
+      "variant": "Regular",
+      "verified": true
+    },
+    {
+      "id": "BT31-010",
+      "number": "BT31-010",
+      "name": "Son Goku & Vegeta, Battling Against Evil",
+      "rarity": "UC",
+      "color": "Red",
+      "type": "BATTLE",
+      "variant": "Regular",
+      "verified": true
+    },
+    {
+      "id": "BT31-011",
+      "number": "BT31-011",
+      "name": "SS Vegeta, Struggle in Another World",
+      "rarity": "C",
+      "color": "Red",
+      "type": "BATTLE",
+      "variant": "Regular",
+      "verified": true
+    },
+    {
+      "id": "BT31-012",
+      "number": "BT31-012",
+      "name": "Vegeta, Hellish Warrior",
+      "rarity": "C",
+      "color": "Red",
+      "type": "BATTLE",
+      "variant": "Regular",
+      "verified": true
+    },
+    {
+      "id": "BT31-013",
+      "number": "BT31-013",
+      "name": "Veku, Incomplete Fusion",
+      "rarity": "UC",
+      "color": "Red",
+      "type": "BATTLE",
+      "variant": "Regular",
+      "verified": true
+    },
+    {
+      "id": "BT31-014",
+      "number": "BT31-014",
+      "name": "SS Gogeta, Revoker of Evil",
+      "rarity": "SR",
+      "color": "Red",
+      "type": "BATTLE",
+      "variant": "Regular",
+      "verified": true
+    },
+    {
+      "id": "BT31-015",
+      "number": "BT31-015",
+      "name": "SS Gogeta, Vanquisher",
+      "rarity": "R",
+      "color": "Red",
+      "type": "BATTLE",
+      "variant": "Regular",
+      "verified": true
+    },
+    {
+      "id": "BT31-016",
+      "number": "BT31-016",
+      "name": "Janemba, Concentrated Hatred",
+      "rarity": "SR",
+      "color": "Red",
+      "type": "BATTLE",
+      "variant": "Regular",
+      "verified": true
+    },
+    {
+      "id": "BT31-017",
+      "number": "BT31-017",
+      "name": "Janemba, Demonic Howl",
+      "rarity": "C",
+      "color": "Red",
+      "type": "BATTLE",
+      "variant": "Regular",
+      "verified": true
+    },
+    {
+      "id": "BT31-018",
+      "number": "BT31-018",
+      "name": "Janemba, Unintentional Evil",
+      "rarity": "UC",
+      "color": "Red",
+      "type": "BATTLE",
+      "variant": "Regular",
+      "verified": true
+    },
+    {
+      "id": "BT31-019",
+      "number": "BT31-019",
+      "name": "Janemba, Little Wicked Clones",
+      "rarity": "C",
+      "color": "Red",
+      "type": "BATTLE",
+      "variant": "Regular",
+      "verified": true
+    },
+    {
+      "id": "BT31-020",
+      "number": "BT31-020",
+      "name": "Baby Janemba, the Abhorrent",
+      "rarity": "SR",
+      "color": "Red",
+      "type": "BATTLE",
+      "variant": "Regular",
+      "verified": true
+    },
+    {
+      "id": "BT31-021",
+      "number": "BT31-021",
+      "name": "Baby Janemba, the Wicked",
+      "rarity": "C",
+      "color": "Red",
+      "type": "BATTLE",
+      "variant": "Regular",
+      "verified": true
+    },
+    {
+      "id": "BT31-022",
+      "number": "BT31-022",
+      "name": "Baby Janemba, Hell Gate Agent of Destruction",
+      "rarity": "R",
+      "color": "Red",
+      "type": "BATTLE",
+      "variant": "Regular",
+      "verified": true
+    },
+    {
+      "id": "BT31-023",
+      "number": "BT31-023",
+      "name": "Janemba, Cyborg Body",
+      "rarity": "R",
+      "color": "Red",
+      "type": "BATTLE",
+      "variant": "Regular",
+      "verified": true
+    },
+    {
+      "id": "BT31-024",
+      "number": "BT31-024",
+      "name": "Power Release",
+      "rarity": "R",
+      "color": "Red",
+      "type": "EXTRA",
+      "variant": "Regular",
+      "verified": true
+    },
+    {
+      "id": "BT31-025",
+      "number": "BT31-025",
+      "name": "Buying Time",
+      "rarity": "C",
+      "color": "Red",
+      "type": "EXTRA",
+      "variant": "Regular",
+      "verified": true
+    },
+    {
+      "id": "BT31-026",
+      "number": "BT31-026",
+      "name": "Hellish Fusion",
+      "rarity": "UC",
+      "color": "Red",
+      "type": "EXTRA",
+      "variant": "Regular",
+      "verified": true
+    },
+    {
+      "id": "BT31-027",
+      "number": "BT31-027",
+      "name": "Monaka",
+      "rarity": "UC",
+      "color": "Blue",
+      "type": "LEADER",
+      "variant": "Regular",
+      "verified": true
+    },
+    {
+      "id": "BT31-028",
+      "number": "BT31-028",
+      "name": "Krillin, Concerned for a God",
+      "rarity": "R",
+      "color": "Blue",
+      "type": "Z-UNISON",
+      "variant": "Regular",
+      "verified": true
+    },
+    {
+      "id": "BT31-029",
+      "number": "BT31-029",
+      "name": "Son Goku, Battle of the Strongest",
+      "rarity": "C",
+      "color": "Blue",
+      "type": "BATTLE",
+      "variant": "Regular",
+      "verified": true
+    },
+    {
+      "id": "BT31-030",
+      "number": "BT31-030",
+      "name": "Son Goku, Thrill of the Fight",
+      "rarity": "C",
+      "color": "Blue",
+      "type": "BATTLE",
+      "variant": "Regular",
+      "verified": true
+    },
+    {
+      "id": "BT31-031",
+      "number": "BT31-031",
+      "name": "Vegeta, Concerned for a God",
+      "rarity": "SR",
+      "color": "Blue",
+      "type": "BATTLE",
+      "variant": "Regular",
+      "verified": true
+    }
+  ]
+};
